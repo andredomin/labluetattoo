80ae290080a02dcd8218a4af54dec6871042ae49
andredomin <andres10dominguez@gmail.com>
2024-01-08 18:24:52 +0100

    update4

--- lablue/src/About.jsx
+++ lablue/src/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
-      const triggerHeightVh = (5000 / window.innerHeight) * 100;
+      const triggerHeightVh = (4000 / window.innerHeight) * 100;
 
       if (scrollPosition > triggerHeightVh && !startAnimation) {
         console.log('funciona')
